c41d5a9c47767ee277419436922050e2e5daab21
Leon Bogaert <leon@tim-online.nl>
2014-05-24 15:08:45 +0200

    A more simple 404 page

--- 404.php
+++ 404.php
@@ -18,8 +18,6 @@ get_header(); ?>
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'hyde' ); ?></p>
 
-					<?php get_search_form(); ?>
-
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
 					<?php if ( hyde_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
@@ -38,12 +36,6 @@ get_header(); ?>
 						</ul>
 					</div><!-- .widget -->
 					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'hyde' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
 
 					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
