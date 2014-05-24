fa7809433cf5b54a3370ac6dbab43fadb25ad5bc
Leon Bogaert <leon@tim-online.nl>
2014-05-24 17:51:13 +0200

    Don't show updated time of a post

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -67,9 +67,6 @@ if ( ! function_exists( 'hyde_posted_on' ) ) :
  */
 function hyde_posted_on() {
 	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-	}
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
